933f298c662c01e82b2d3f0db0bb07db77e53250
7Stvn0 <stevenn022007@gmail.com>
2026-09-21 21:59:20 -0500

    feat(login): New login function with your template, URL, CSS, JS

--- login/urls.py
+++ login/urls.py
@@ -1,7 +1,9 @@
 
 from app.urls import auth_views
 from django.urls import path
+
+
 urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name = 'inicio_sesion.html'), name = 'login'),
-    
+    path('password-reset/',auth_views.PasswordResetView.as_view(template_name='login/password_reset.html'), name='password_reset'),
 ]
