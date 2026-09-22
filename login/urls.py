7caae958cea2848d69399eb60002f9de62d033f0
Stefy705 <stefhguz702@gmail.com>
2026-09-22 08:22:01 -0500

    feat(login): Add new styles to records

--- login/urls.py
+++ login/urls.py
@@ -5,6 +5,6 @@ from app.urls import auth_views
 from django.urls import path
 urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name = 'inicio_sesion.html'), name = 'login'),
-    path('registro/', views.registro, name='registro')
+    path('registro/', views.registro, name='registro'),
     
 ]
