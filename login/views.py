c49b0286cc9508c837175642a3f67b9748254d97
7Stvn0 <stevenn022007@gmail.com>
2026-09-24 08:56:05 -0500

    feat:(login): I made changes to `urls.py` and the ´Inicio_sesion´ template, and added new functions to `views.py`.

--- login/views.py
+++ login/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from usuarios.models import Usuario
+
+
+@method_decorator(login_required, name='dispatch')
+class PerfilUsuarioView(DetailView):
+    model = Usuario
+    template_name = 'login/perfil.html'
+    context_object_name = 'usuario'
+
+    def get_object(self):
+        return self.request.user
 
 def registro(request):
     return render(request, 'registro.html')
