4235f9a8f2c50e133a2aa48a3d3de6904eefd2ec
itskx01 <ronalttalarcon@gmail.com>
2026-09-24 07:57:34 -0500

    feat(login): implementa el flujo completo de recuperacion de contrasena con vistas, plantillas y estilos

--- login/urls.py
+++ login/urls.py
@@ -1,12 +1,12 @@
-
-from . import views
-
+from django import views
 from app.urls import auth_views
 from django.urls import path
-
+from . import views 
 
 urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name = 'inicio_sesion.html'), name = 'login'),
     path('registro/', views.registro, name='registro'),
-    path('password-reset/',auth_views.PasswordResetView.as_view(template_name='login/password_reset.html'), name='password_reset'),
+    path('email/', views.email_request_view, name='email_request'),
+    path('password-request/', views.codigo_verif_view, name='password_request'),
+    path('password-reset/', views.nueva_contrasena_view, name='password_new'),
 ]
